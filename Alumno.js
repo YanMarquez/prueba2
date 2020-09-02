dc84d771607578775604462e9ac95e2cdfd0d01f
YanMarquez <70238924+YanMarquez@users.noreply.github.com>
2020-09-02 00:02:44 -0500

    Add files via upload

---
+++ Alumno.js
@@ -0,0 +1,19 @@
+
+//---mnasaje de alerta---
+//alert('hola')
+//para ingresar datos 
+//
+function saludo(){
+    var n1;
+    n1= prompt('hola','Ingresa tu nombre');
+    alert("Hola: "+n1)  
+}
+
+function salida(){
+    alert('Que tenga un exclente dia')
+}
+
+//function saludo(){
+//alert('hola')
+//alert('hola')
+//}
